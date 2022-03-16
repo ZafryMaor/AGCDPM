6c7d796e03fe19c666eea4dea66c51c011de6ca1
ZafryMaor <94444890+ZafryMaor@users.noreply.github.com>
2022-03-16 11:34:45 +0200

    Add files via upload

---
+++ inc/DBAMiner.h
@@ -0,0 +1,17 @@
+#ifndef DBAMINER_H
+#define DBAMINER_H
+
+#include "SimpleStructures.h"
+#include "InputHandler.h"
+#include "Miner.h"
+#include "OutputHandler.h"
+#include "ErrorPrinter.h"
+
+#include <string>
+#include <vector>
+#include <map>
+#include <stdio.h>
+#include <iostream>
+#include <atomic>
+
+#endif // !DBAMINER_H
